4b42c7af8336759f053600f7160af06a3c4416d3
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-15 16:13:01 +1300

    Add responsiveness for different screen sizes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
             <div className="main-container" id="about">
                 <img src={sammy} alt="a picture of me!" className="main-pic" />
                 <div className="about-me">
-                    <h2>About Me</h2>
+                    <h2 className="about-header">About Me</h2>
                     <p>
                     I am a full-stack developer with a keen interest in using my skills to work on meaningful projects with a positive real-world impact.
                     </p>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 
 function Nav () {
+
     return(
+       <> 
+            {/* <script>
+                const burger = document.querySelector("#burger-menu");
+                const ul = document.querySelector("nav ul");
+                const nav = document.querySelector("nav");
+
+                burger.addEventListener("click", () => {
+                    ul.classList.toggle("show");
+                });
+            </script> */}
+    
+        
+
         <div className="header-container">
             <h1 className="header-name">Sam Ward-Pickering</h1>
             <ul className="link-list">
@@ -12,10 +27,11 @@ function Nav () {
                 <li><a href="#contact" className="nav-link">Contact</a></li>
             </ul>
             <button className="burger-menu" id="burger-menu">
-                
+                <GiHamburgerMenu/>
             </button>
             
         </div>
+       </>
     )
 }
 
