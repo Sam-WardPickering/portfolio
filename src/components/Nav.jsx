40a612bf4e691c9d7985155cad9c6235d0c40883
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-17 13:24:51 +1300

    Styling + page icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,11 +8,11 @@ function Nav () {
     return(
        <> 
         <div className="header-container">
-            <h1 className="header-name">Sam Ward-Pickering</h1>
+            <a className="name-link" href="#"><h1 className="header-name">Sam Ward-Pickering</h1></a>
             <ul className="link-list">
                 {/* <li><a href="#about" className="nav-link">About</a></li> */}
                 <li><a href="#projects" className="nav-link">Projects</a></li>
-                
+
                 <li><a href="#skills" className="nav-link">Skills</a></li>
                 
                 <li><a href="#contact" className="nav-link">Contact</a></li>
